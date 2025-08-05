ff4d83a8f13b545434311bcfc6931bb0b5c35d55
zoena5303 <smile1997590@gmail.com>
2025-08-05 15:40:33 +0800

    更

--- react-project/src/App0805-2.jsx
+++ react-project/src/App0805-2.jsx
@@ -1,8 +1,35 @@
-function App(){
+import React from "react";
 
+function App() {
+  return (
+    <div className="news-container">
+      <h1 className="news-title">
+        日本民宿《寶可夢》卡包裝垃圾堆成山！業者曬照「簡中版」被網推測是中國人
+      </h1>
 
+      <div className="news-meta">
+        Yahoo奇摩新聞・2025年8月5日 07:52
+      </div>
 
+      <img
+        className="news-image"
+        src="https://s.yimg.com/ny/api/res/1.2/SigBEQ6R0HUIl05lzvPRjA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTcwNTtoPTUyMw--/https://media.zenfs.com/zh-tw/popbee_422/6f5a8eafde65ab6d20e2ddf9cb2b2733"
+        alt="寶可夢垃圾照"
+      />
 
+      <p className="news-paragraph">
+        隨著《寶可夢》卡牌席捲全球，世界各地的訓練家們紛紛投身卡牌蒐集，不僅官方卡牌競賽越來越熱絡，玩家也經常舉辦私人交換活動。然而，近日日本一間民宿業者卻在社群上公開控訴，有房客在退房後留下大量《寶可夢》卡包裝垃圾堆積成山，甚至明顯可見「簡體中文」標示，掀起熱議。
+      </p>
 
-    
+      <p className="news-paragraph">
+        業者無奈地表示，房客疑似在房內開箱數百包卡牌，只留下包裝和塑膠袋，完全未處理垃圾，引發他們清潔上的困擾。由於卡包外盒上的語言標示為簡體中文，也被不少網友猜測此行為者可能是中國旅客。
+      </p>
+
+      <p className="news-paragraph">
+        此事件引起社群熱議，有人呼籲加強房客素質，也有人建議業者可在房內張貼多語言垃圾處理規定。無論真相如何，垃圾問題也再次提醒大眾，尊重環境與公共空間是全球旅遊基本素養。
+      </p>
+    </div>
+  );
 }
+
+export default App;
--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,11 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 //import App from './App.jsx'
-import App from './App0805-1.jsx'
-import App from './App0805-2.jsx'
+//import App from './App0805-1.jsx'
+//import './css./App.css'
+//import App from './App0805-2.jsx'
+import "./css/App.css";
+import App from "./App0805-2.jsx";
+
 createRoot(document.getElementById('root')).render(
   //嚴謹模式
 <StrictMode>
     <App />
   </StrictMode>,
 )
+
+App.css
