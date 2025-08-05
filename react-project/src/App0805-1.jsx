3c7f73716b0eb8cfbedda1eba2521c6c1fdc1594
zoena5303 <smile1997590@gmail.com>
2025-08-05 14:41:12 +0800

    0805

---
+++ react-project/src/App0805-1.jsx
@@ -0,0 +1,23 @@
+import'./css/App0805-1.css'
+
+
+function App(){
+    //一般變數
+    const myName="阿娟"
+    //屬性名稱變數
+    const myclassName="myClassName"
+    //方法
+    function myClick(){
+        alert('我是按鈕1')
+    }
+    return(
+       <>
+    <div>
+        <p className={myClassName}>姓名:{myName}</p>
+        <button type="button" onClick={myClick}>按鈕1</button>
+         <button type="button" onClick={myClick}>按鈕2</button>
+    </div>
+       </>
+    )
+}
+export default App;
--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+//import './index.css'
+//import App from './App.jsx'
+import App from './App0805-1.jsx'
+import App from './App0805-2.jsx'
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //嚴謹模式
+<StrictMode>
     <App />
   </StrictMode>,
 )
