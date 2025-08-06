94bd8402aedeab93cbfd2e54258e04d7d3eb565d
zoena5303 <smile1997590@gmail.com>
2025-08-06 20:55:48 +0800

    更正

--- react-project/src/pages/App0806-1.jsx
+++ react-project/src/pages/App0806-1.jsx
@@ -3,7 +3,7 @@ const App = () => {
   return (
     <>
       <div className="card" style={{width:"18rem"}}>
-        <img src="" className="card-img-top" alt="..."/>
+        <img src="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="狗狗奔跑"/>
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
