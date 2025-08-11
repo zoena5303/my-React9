04d23080f23940bede8bb88bf769bf1d4b8e9f04
zoena5303 <smile1997590@gmail.com>
2025-08-11 09:09:47 +0800

    1

--- react-project/src/pages/App0806-1.jsx
+++ react-project/src/pages/App0806-1.jsx
@@ -1,48 +1,40 @@
-/* import React from 'react' */
 import '../css/app0806-1.css'
 
 const App = () => {
-  // 資料抽離
-  const photos = {
-    photo1: {
-      imgURL: 'https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      title: '奔跑的狗狗',
-      desc: "狗狗在草地上快樂奔跑，畫面生動可愛！",
-      btnName: '看大圖',
-      btnURL: 'https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?q=80&w=688'
-    },
-    photo2: {
-      imgURL: 'https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=662&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      title: '笑笑的阿金',
-      desc: '開心的黃金獵犬坐在草地上，笑容超療癒！',
-      btnName: '前往連結',
-      btnURL: '#'
-    },
-    photo3: {
-      imgURL: 'https://images.unsplash.com/photo-1510771463146-e89e6e86560e?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      title: '咬花花的狗狗',
-      desc: '狗狗叼著花，表情無辜，畫面超萌！',
-      btnName: '看更多',
-      btnURL: '#'
-    }
-  }
-
-  return (
-    <>
-      <div className="container" style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
-        {Object.values(photos).map((item, index) => (
-          <div className="card" style={{ width: '18rem' }} key={index}>
-            <img src={photos.photo1.imgURL} className="card-img-top" alt={item.title} />
-            <div className="card-body">
-              <h5 className="card-title">{item.title}</h5>
-              <p className="card-text">{item.desc}</p>
-              <a href={item.btnURL} className="btn btn-primary">{item.btnName}</a>
+    return (
+        <>
+        <div className="container">
+            <div className="card">
+                <img src="https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?_gl=1*1hvyrbz*_ga*MTk1ODE0NTg3Ni4xNzU0NDQzNDYw*_ga_8JE65Q40S6*czE3NTQ0NDM0NTkkbzEkZzEkdDE3NTQ0NDM0ODkkajMwJGwwJGgw" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn">Go somewhere</a>
+                    </div>
             </div>
-          </div>
-        ))}
-      </div>
-    </>
-  )
+
+            <div className="card">
+                <img src="https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?_gl=1*1hvyrbz*_ga*MTk1ODE0NTg3Ni4xNzU0NDQzNDYw*_ga_8JE65Q40S6*czE3NTQ0NDM0NTkkbzEkZzEkdDE3NTQ0NDM0ODkkajMwJGwwJGgw" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn">Go somewhere</a>
+                    </div>
+            </div>
+
+            <div className="card">
+                <img src="https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?_gl=1*1hvyrbz*_ga*MTk1ODE0NTg3Ni4xNzU0NDQzNDYw*_ga_8JE65Q40S6*czE3NTQ0NDM0NTkkbzEkZzEkdDE3NTQ0NDM0ODkkajMwJGwwJGgw" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn">Go somewhere</a>
+                    </div>
+            </div>
+        </div>
+            
+
+            </>
+    )
 }
 
 export default App;
